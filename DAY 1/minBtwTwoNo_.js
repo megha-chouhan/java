d6c3d5c4ee6c589956737dea7c6015549483f6f2
Megha <developermegha98@gmail.com>
2023-06-15 21:36:08 +0530

    minimum between two number

---
+++ DAY 1/minBtwTwoNo_.js
@@ -0,0 +1,12 @@
+function minBwTwoNo(num1, num2) {
+  if (num1 < num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+let number1 = 23;
+let number2 = 45;
+let answer = minBwTwoNo(number1, number2);
+console.log(`min bw ${number1} and ${number2} is - `, answer);
